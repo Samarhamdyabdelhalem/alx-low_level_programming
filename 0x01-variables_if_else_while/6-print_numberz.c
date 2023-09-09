0414d8ec93b2ab580c77e3ab9866395903a6834f
samarhamdyabdelhalem <samarabdelhalim1@gmail.com>
2023-09-09 11:26:42 -0700

    66

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,23 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: print 0 - 9 using putchar
- * 		while using int variable
+ * Description: print numbers from 0 to 9
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int digit = 0;
+	int i;
 
-	while (digit <= 9)
+	for (i = 48; i < 58; i++)
 	{
-		/*convert digit to ASCII representation*/
-		putchar(digit + '0');
-		digit++;
+		putchar(i);
 	}
 	putchar('\n');
-
 	return (0);
 }
